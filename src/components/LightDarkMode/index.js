5c444bb330e899c531ceafe0045cebbe34abdd4d
Surya T N <suryatej.nim@yahoo.com>
2022-08-31 17:22:21 +0000

    Adjusted styling of elements and implemented button click handler for mode toggle in LightDarkMode component

--- src/components/LightDarkMode/index.js
+++ src/components/LightDarkMode/index.js
@@ -6,14 +6,43 @@ export default class LightDarkMode extends Component {
     currentMode: 'Dark Mode',
   }
 
-  onRender() {
+  onModeToggle = () => {
+    let {currentMode} = this.state
+    if (currentMode === 'Dark Mode') {
+      currentMode = 'Light Mode'
+    } else {
+      currentMode = 'Dark Mode'
+    }
+
+    this.setState({currentMode})
+  }
+
+  render() {
     const {currentMode} = this.state
 
     return (
       <div className="light-dark-mode-bg-container">
-        <div className="content-container">
-          <h1 className="content-header">Click To Change Mode</h1>
-          <button type="button" className="mode-toggle-button">
+        <div
+          className={
+            currentMode === 'Dark Mode'
+              ? 'content-container'
+              : 'content-container light-mode-content-container'
+          }
+        >
+          <h1
+            className={
+              currentMode === 'Dark Mode'
+                ? 'content-header'
+                : 'content-header light-mode-content-header'
+            }
+          >
+            Click To Change Mode
+          </h1>
+          <button
+            type="button"
+            className="mode-toggle-button"
+            onClick={this.onModeToggle}
+          >
             {currentMode === 'Dark Mode' ? 'Light Mode' : 'Dark Mode'}
           </button>
         </div>
