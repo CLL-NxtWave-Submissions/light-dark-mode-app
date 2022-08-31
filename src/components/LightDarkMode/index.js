76509cddd9be3dcd0538be143b3b847781cb61d0
Surya T N <suryatej.nim@yahoo.com>
2022-08-31 16:54:59 +0000

    Implemented LightDarkMode component code without the button click event handler in corresponding component's index.js file

--- src/components/LightDarkMode/index.js
+++ src/components/LightDarkMode/index.js
@@ -1,1 +1,23 @@
-// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+export default class LightDarkMode extends Component {
+  state = {
+    currentMode: 'Dark Mode',
+  }
+
+  onRender() {
+    const {currentMode} = this.state
+
+    return (
+      <div className="light-dark-mode-bg-container">
+        <div className="content-container">
+          <h1 className="content-header">Click To Change Mode</h1>
+          <button type="button" className="mode-toggle-button">
+            {currentMode === 'Dark Mode' ? 'Light Mode' : 'Dark Mode'}
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
